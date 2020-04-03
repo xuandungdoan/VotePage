4d00b5037d8c2d19e68156dece65b8a1925e1df1
Doan Xuan Dung <xuandung.cnc@gmail.com>
2020-04-03 23:43:46 +0700

    fix format CreatePoll component

--- client/src/components/CreatePoll.jsx
+++ client/src/components/CreatePoll.jsx
@@ -1,6 +1,5 @@
 import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
-
 import { createPoll } from "../store/actions";
 
 class CreatePoll extends Component {
@@ -10,46 +9,39 @@ class CreatePoll extends Component {
       question: "",
       options: ["", ""]
     };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.addAnswer = this.addAnswer.bind(this);
-    this.handleAnswer = this.handleAnswer.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
-
-  handleChange(e) {
+  handleChange = e => {
     this.setState({ [e.target.name]: e.target.value });
-  }
-
-  addAnswer() {
+  };
+  addAnwser = () => {
     this.setState({ options: [...this.state.options, ""] });
-  }
-
-  handleAnswer(e, index) {
+  };
+  handleOption = (e, index) => {
     const options = [...this.state.options];
     options[index] = e.target.value;
     this.setState({ options });
-  }
-
-  handleSubmit(e) {
+  };
+  handleSumit = e => {
     e.preventDefault();
     this.props.createPoll(this.state);
-  }
+  };
 
   render() {
-    const options = this.state.options.map((option, index) => (
-      <Fragment key={index}>
-        <label htmlFor="options">Options</label>
-        <input
-          type="text"
-          name="options"
-          value={option}
-          onChange={e => this.handleAnswer(e, index)}
-        />
-      </Fragment>
-    ));
+    const options = this.state.options.map((option, index) => {
+      return (
+        <Fragment key={index}>
+          <label htmlFor="options">Options</label>
+          <input
+            type="text"
+            name="options"
+            value={option}
+            onChange={e => this.handleOption(e, index)}
+          />
+        </Fragment>
+      );
+    });
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={this.handleSumit}>
         <label htmlFor="question">Question</label>
         <input
           type="text"
@@ -58,7 +50,7 @@ class CreatePoll extends Component {
           onChange={this.handleChange}
         />
         {options}
-        <button onClick={this.addAnswer} type="button">
+        <button type="button" onClick={this.addAnwser}>
           Add Answer
         </button>
         <button type="submit">Submit</button>
@@ -68,82 +60,3 @@ class CreatePoll extends Component {
 }
 
 export default connect(() => ({}), { createPoll })(CreatePoll);
-// import React, { Component, Fragment } from "react";
-// import { connect } from "react-redux";
-
-// import { createPoll } from "../store/actions";
-
-// class CreatePoll extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       question: "",
-//       options: ["", ""]
-//     };
-
-//     this.handleChange = this.handleChange.bind(this);
-//     this.addAnswer = this.addAnswer.bind(this);
-//     this.handleAnswer = this.handleAnswer.bind(this);
-//     this.handleSubmit = this.handleSubmit.bind(this);
-//   }
-
-//   handleChange(e) {
-//     this.setState({ [e.target.name]: e.target.value });
-//   }
-
-//   addAnswer() {
-//     this.setState({ options: [...this.state.options, ""] });
-//   }
-
-//   handleAnswer(e, index) {
-//     const options = [...this.state.options];
-//     options[index] = e.target.value;
-//     this.setState({ options });
-//   }
-
-//   handleSubmit(e) {
-//     e.preventDefault();
-//     this.props.createPoll(this.state);
-//   }
-
-//   render() {
-//     const options = this.state.options.map((option, i) => (
-//       <Fragment>
-//         <label className="form-label">option</label>
-//         <input
-//           className="form-input"
-//           type="text"
-//           value={option}
-//           key={i}
-//           onChange={e => this.handleAnswer(e, i)}
-//         />
-//       </Fragment>
-//     ));
-
-//     return (
-//       <form className="form" onSubmit={this.handleSubmit}>
-//         <label className="form-label" htmlFor="question">
-//           question
-//         </label>
-//         <input
-//           className="form-input"
-//           type="text"
-//           name="question"
-//           value={this.state.question}
-//           onChange={this.handleChange}
-//         />
-//         <div className="container">{options}</div>
-//         <div className="buttons_center">
-//           <button className="button" type="button" onClick={this.addAnswer}>
-//             Add options
-//           </button>
-//           <button className="button" type="submit">
-//             Submit
-//           </button>
-//         </div>
-//       </form>
-//     );
-//   }
-// }
-
-// export default connect(() => ({}), { createPoll })(CreatePoll);
